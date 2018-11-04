7807a81b87556bdfe8db7c876ff4549e529f4b41
Dalibor Randelovic <perasic@gmail.com>
2018-11-04 00:59:53 +0000

    Added some cool stuff
    color transition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var doSomeCoolStuff = function () {
+	// changing class name
+	var currentClassName = document.getElementById('cool').className;
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	
+}
+
 var sayMyName = function (name) {
 	alert('My name is: '+name);
 }
